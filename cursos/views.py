136d197865a2682387f2c997bb641ed7c2b5b694
Luis-AP <lap18958@gmail.com>
2024-03-27 22:35:41 -0300

    Vista renderizando plantilla base

--- cursos/views.py
+++ cursos/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(
+        request,
+        "base.html",
+        context={"li_01": "HOME", "li_02": "CURSOS", "li_03": "estudiantes"},
+    )
